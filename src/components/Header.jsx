90982a440ade65caf3d281fda6784bb4f0dcb7f9
Joseangvas <developer.javp@gmail.com>
2024-03-27 18:07:08 -0400

    Codificar Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
                     <div className="col-8 col-md-3">
                         <a href="index.html">
                             <img
-                            className="img-fluid"
-                            src="./public/img/logo.svg"
-                            alt="imagen logo"
+                                className="img-fluid"
+                                src="./public/img/logo.svg"
+                                alt="imagen logo"
                             />
                         </a>
                     </div>
@@ -17,49 +17,50 @@ function Header() {
 
                         <div className="carrito">
                             <img
-                            className="img-fluid"
-                            src="./public/img/carrito.png"
-                            alt="imagen carrito"
+                                className="img-fluid"
+                                src="./public/img/carrito.png"
+                                alt="imagen carrito"
                             />
 
                             <div id="carrito" className="bg-white p-3">
                                 <p className="text-center">El carrito esta vacio</p>
                                 <table className="w-100 table">
                                     <thead>
-                                    <tr>
-                                        <th>Imagen</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Cantidad</th>
-                                        <th></th>
-                                    </tr>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
                                     </thead>
+
                                     <tbody>
-                                    <tr>
-                                        <td>
-                                        <img
-                                            className="img-fluid"
-                                            src="./public/img/guitarra_02.jpg"
-                                            alt="imagen guitarra"
-                                        />
-                                        </td>
-                                        <td>SRV</td>
-                                        <td className="fw-bold">$299</td>
-                                        <td className="flex align-items-start gap-4">
-                                        <button type="button" className="btn btn-dark">
-                                            -
-                                        </button>
-                                        1
-                                        <button type="button" className="btn btn-dark">
-                                            +
-                                        </button>
-                                        </td>
-                                        <td>
-                                        <button className="btn btn-danger" type="button">
-                                            X
-                                        </button>
-                                        </td>
-                                    </tr>
+                                        <tr>
+                                            <td>
+                                            <img
+                                                className="img-fluid"
+                                                src="./public/img/guitarra_02.jpg"
+                                                alt="imagen guitarra"
+                                            />
+                                            </td>
+                                            <td>SRV</td>
+                                            <td className="fw-bold">$299</td>
+                                            <td className="flex align-items-start gap-4">
+                                            <button type="button" className="btn btn-dark">
+                                                -
+                                            </button>
+                                            1
+                                            <button type="button" className="btn btn-dark">
+                                                +
+                                            </button>
+                                            </td>
+                                            <td>
+                                            <button className="btn btn-danger" type="button">
+                                                X
+                                            </button>
+                                            </td>
+                                        </tr>
                                     </tbody>
                                 </table>
 
