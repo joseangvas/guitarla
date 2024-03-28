2e15e8bd3488f5043cacb2d887554e87b14d2501
Joseangvas <developer.javp@gmail.com>
2024-03-28 12:25:19 -0400

    Codificar Evento Click en Botón Agregar al Carrito.

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -15,7 +15,11 @@ export default function Guitar({guitar}) {
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button 
+          type="button"
+          className="btn btn-dark w-100"
+          onClick={handleClick}
+          >
           Agregar al Carrito
         </button>
       </div>
